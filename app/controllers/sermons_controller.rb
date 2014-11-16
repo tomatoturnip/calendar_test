26ab71f4c4af1213c026e1cc7369f5f0d36c0a86
Lydia Ho <lyd.w.ho@gmail.com>
2014-11-16 16:20:16 -0500

    Add actions to the sermons controller

--- app/controllers/sermons_controller.rb
+++ app/controllers/sermons_controller.rb
@@ -1,5 +1,32 @@
 class SermonsController < ApplicationController
   def index
+    @sermons = Sermon.all
   end
 
+  def new
+    @sermon = Sermon.new
+  end
+
+  def create
+    @sermon = Sermon.new(sermon_params)
+
+    if @sermon.save
+      redirect_to sermons_path, notice: "The sermon has been uploaded."
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @sermon = Sermon.find(params[:id])
+    @sermon.destroy
+    redirect_to sermons_path, notice: "The sermon has been deleted."
+  end
+
+  private
+    def sermon_params
+      params.require(:sermon).permit(:name, :subject, :day, :avatar)
+    end
 end
+
+
