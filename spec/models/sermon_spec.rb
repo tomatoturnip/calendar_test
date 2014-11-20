bcce0554172dc8a588a7c10d8de268e8509af74b
Lydia Ho <lyd.w.ho@gmail.com>
2014-11-20 17:18:08 -0500

    Add validation tests

--- spec/models/sermon_spec.rb
+++ spec/models/sermon_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Sermon, :type => :model do
 
-  it "should have a speaker" do
-    sermon = Sermon.new
-    expect(sermon.speaker).to eq("Jones")
-  end
+  it { should validate_presence_of(:speaker) }
+  it { should validate_presence_of(:subject) }
+  it { should validate_presence_of(:day) }
+
+
+
+
 end
