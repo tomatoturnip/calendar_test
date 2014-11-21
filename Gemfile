ae34a3f2f952e20aa38dc6beadb5f5f791dcd0b5
Lydia Ho <lyd.w.ho@gmail.com>
2014-11-21 16:58:03 -0500

    Add capybara webkit for integration testing

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ end
 group :development, :test do
   gem 'rspec-rails', '~> 3.0'
   gem 'capybara'
+  gem 'capybara-webkit'
   gem 'factory_girl_rails'
 end
 
--- spec/models/sermon_spec.rb
+++ spec/models/sermon_spec.rb
@@ -6,7 +6,4 @@ RSpec.describe Sermon, :type => :model do
   it { should validate_presence_of(:subject) }
   it { should validate_presence_of(:day) }
 
-
-
-
 end
