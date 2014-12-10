fb51f77331738d2c08d711b5d853dd5bbd47cdb2
Lydia Ho <lyd.w.ho@gmail.com>
2014-12-10 15:34:39 -0500

    Add admin-approved methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,36 @@
 class User < ActiveRecord::Base
+  after_create :send_admin_mail
+
   ROLES = ["member", "admin"]
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def active_for_authentication?
+    super && approved?
+  end
+
+  def inactive_message
+    if !approved?
+      :not_approved
+    else
+      super
+    end
+  end
+
+  def send_admin_mail
+    AdminMailer.new_user_waiting_for_approval(self).deliver
+  end
+
+  def self.send_reset_password_instructions(attributes={})
+    recoverable = find_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+    if !recoverable.approved?
+      recoverable.errors[:base] << I18n.t("devise.failure.not_approved")
+    elsif recoverable.persisted?
+      recoverable.send_reset_password_instructions
+    end
+    recoverable
+  end
 end
