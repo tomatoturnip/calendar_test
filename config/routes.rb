361f041946c4215428807dd2f684759feb327e89
Lydia Ho <lyd.w.ho@gmail.com>
2015-01-14 15:12:39 -0500

    Remove fullcalendar rails gem from namespaces admin and member

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   namespace :admin do
-    mount FullcalendarEngine::Engine => "/calendar"
     resources :sermons, except: :show
     root to: 'home#index'
   end
 
   namespace :member do
-    mount FullcalendarEngine::Engine => "/calendar"
     resources :sermons, except: :show
     root to: 'home#index'
   end
 
+  mount FullcalendarEngine::Engine => "/calendar"
   devise_for :users
   root 'welcome#index'
 end
