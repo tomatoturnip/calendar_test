a51a8465104bc1840c1f2590117c3969d2944ebe
Lydia Ho <lyd.w.ho@gmail.com>
2015-01-26 17:28:23 -0500

    Add admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,8 +18,6 @@ Sermon.create(speaker: "foo8", subject: "bar8", day: "2014-11-27", avatar: nil)
 
 # Users
 puts "Seeding users..."
-User.create(email: 'lydia@barbershoplabs.com', password: 'barbershop', password_confirmation: 'barbershop', role: 'admin', approved: true)
-User.create(email: 'ho9708@gmail.com', password: 'barbershop', password_confirmation: 'barbershop', role: 'admin', approved: true)
-User.create(email: 'testuser@test.com', password: 'barbershop', password_confirmation: 'barbershop', role: 'member', approved: true)
+User.create(email: 'peninsulachurchinchrist@gmail.com', password: ENV['ADMIN_PASSWORD'], password_confirmation: '596 Mercy', role: 'admin', approved: true)
 
 puts "Done seeding."
