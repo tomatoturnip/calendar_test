54bd707a7f7e59403298d88fa30499a0e07ee249
Lydia Ho <lyd.w.ho@gmail.com>
2015-01-26 19:14:55 -0500

    Add actions to admin users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,4 +9,55 @@ class Admin::UsersController < ApplicationController
     end
   end
 
+  def new
+    User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+
+        format.html { redirect_to admin_users_path, notice: "User created" }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(user_params)
+        format.html { redirect_to admin_users_path, notice: "User updated." }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    resond_to do |format|
+      format.html { redirect_to admin_users_path, notice: "User deleted." }
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :role, :approved)
+  end
 end
