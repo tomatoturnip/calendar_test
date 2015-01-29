05c770ee5d10f83c46233f8bbce85e6d5ad264ae
Lydia Ho <lyd.w.ho@gmail.com>
2015-01-29 15:06:53 -0500

    Edit email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,9 @@ Rails.application.configure do
     :address                => "stmp.mandrillapp.com",
     :port                   => "587",
     :user_name              => ENV["MANDRILL_USERNAME"],
-    :password               => ENV["MANDRILL_APIKEY"]
+    :password               => ENV["MANDRILL_APIKEY"],
+    :domain                 => 'heroku.com',
+    :authentication         => :plain
   }
+  ActionMailer::Base.delivery_method = :smtp
 end
